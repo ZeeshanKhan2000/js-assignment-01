697602b7ee12f4f9fa8d2a458e891e52d5802abc
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2022-11-01 12:24:00 -0700

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,22 @@
+// alert ("welcome to javascript");
+
+// alert ("put your cooment here");
+
+// alert ("fill the form first");
+
+// alert ("learn js basic");
+
+
+// alert ("basic javascript");
+
+// alert ("jawan pakistan");
+
+
+// alert ("message info") ;
+
+// alert ("course outline");
+
+// alert ("thanks for the input");
+
+// alert ("zeeshan afzal");
+
---
+++ js/variable.js
@@ -0,0 +1,247 @@
+// variable portion
+
+var Name ="jawan pakistan";
+// alert (Name);
+
+var abc ="javascript";
+// alert (abc);
+
+var ab123 ="basic javascript";
+// alert (ab123);
+
+var ab334bs ="learn javascript"
+// alert (ab334bs);
+
+var LastName ="hello world";
+// alert (LastName);
+
+var FisrtName ="pakistan zindaband";
+// alert (FisrtName);
+
+var ads34an ="google"
+// alert (ads34an);
+
+// console.log(typeof(FisrtName))
+
+                // *****************     variable   in number   *********
+
+var acb = 123;
+// console.log(acb);
+// alert (acb);
+
+var gega =20+10;
+// console.log(gega);
+// alert (gega);
+
+var apple =29*10;
+// console.log(apple);
+// alert (apple);
+
+ var turtle =20-2;
+// console.log(turtle);
+// alert (turtle);
+
+var cargo =30/50;
+// console.log(cargo);
+// alert (cargo);
+
+var car = 40/10;
+// console.log(car);
+// alert (car);
+
+var purple = 15-5;
+// console.log(purple);
+// alert (purple);
+
+var orange =30+20;
+// console.log(orange);
+// alert (orange);
+
+var Name="zeeshan";
+var age12 ="age"
+var age=30;
+var newvalue= (Name+age);
+// console.log( newvalue);
+
+// alert( "My name is " +  Name + " \n"+"My age is " + age);
+
+ var message = "Thanks, ";
+ var userName = "Susan";
+ var banger = "!";
+ var customMess = message + userName + banger;
+//  alert(customMess);
+
+// ***********************************legal  and  illegal variable**********
+
+
+//   legal variable
+
+ var $king =10;
+var flower =20;
+var _abc123 ="pakistan";
+var bcbc_abc ="hello";
+var userResponse ="world"
+var FOR = "world";
+
+
+// ************************************illegal variable***************
+
+
+// var 123 = "hello";
+// var /123 = "hello";
+// var juk box = "hello";
+// var Number = "hello";
+
+// keyword cant be use as a variable
+ 
+
+
+
+// *********************************  Math expressions:**************
+
+
+var num = 10;
+var anotherNum = 1;
+var popularNumber = num + anotherNum;
+
+
+// alert (popularNumber)
+
+// *************example*************
+
+var popularNumber = 4
+
+// But you can also write this.
+
+var popularNumber = 2 + 2;
+
+
+// You can also write:
+
+
+// alert(2 + 2);
+
+var numPower =10;
+
+var numDown =100;
+
+var newNum =(numPower+100)
+
+
+// console.log(newNum)
+
+// alert (newNum)
+
+
+
+
+// ***************************Unfamiliar operators********************
+
+var num =1
+
+// console.log(num++)
+// alert (num)
+
+var abcd = 20;
+
+
+// console.log(abcd--)
+
+
+
+
+// document.write(abcd)
+// document.write("<br />")
+
+
+
+// document.write(--abcd)
+// document.write("<br />")
+// document.write(abcd--)
+// document.write("<br />")
+// document.write(abcd)
+
+
+// ******************************  Math expressions:Eliminating ambiguity*************
+
+var totalCost = 1+2*4
+
+// console.log(totalCost)
+// alert (totalCost)
+
+var totalNum = 20-30/2
+
+// console.log(totalNum )
+// alert (totalNum )
+
+
+var acv =60+4-2*6;
+
+// console.log(acv)
+// alert (acv )
+
+
+var afgb =6+40-20*66;
+
+// console.log(afgb )
+// alert (afgb)
+
+
+
+var acv9 =699+4-92*699;
+
+// console.log(acv9)
+// alert (acv9 )
+
+
+var acv55 =608+40/254*666;
+
+// console.log(acv55)
+// alert (acv55)
+
+
+var acv112 =60+42*6;
+
+// console.log(acv112)
+// alert (acv112)
+
+var acv00 =60+7784/2*0.06;
+
+// console.log(acv00)
+// alert (acv00 )
+
+
+
+// *********************Concatenating text strings*****************
+
+// var massege = ("thanks for your input")
+
+
+// var acnd = ("hello world")
+
+
+// var abc =("basic javascript")
+
+// var bbb =("welcome to js")
+
+// alert(bbb)
+
+
+
+// zeeshan = "Afzal Khan"
+// console.log(zeeshan) //chalygaa 
+
+
+
+// var lumber1 = 5;
+// var lumber2 = "5";
+// console.log(lumber1+lumber2);  // 5
+
+
+var banger =("zeeshan afzal")
+
+var age =50
+
+// var newVariableName=("my name is " + banger+ " \n "+ "my age is " + age)
+
+// alert(newVariableName)
